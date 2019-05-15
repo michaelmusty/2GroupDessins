f1da498351530e19830babfb1bf46f85aecfee21
michaelmusty <michaelmusty@gmail.com>
2019-05-15 13:50:33 -0400

    more examples

--- scripts/051519.m
+++ scripts/051519.m
@@ -4,22 +4,21 @@ load "config.m";
 q := 3^2;
 FFq := GF(q);
 
-s16 := ReadTwoDB(Random(Filenames(16)));
-path := PathToPP1(s16);
-s2 := path[1];
-s4 := path[2];
-s8 := path[3];
+/* s16 := ReadTwoDB(Random(Filenames(16))); */
+/* path := PathToPP1(s16); */
+/* s2 := path[1]; */
+/* s4 := path[2]; */
+/* s8 := path[3]; */
 
-s2 := ReadTwoDB("2T1-2,2,1-g0-206495183.m");
-s4 := ReadTwoDB("4T1-4,4,2-g1-1323523021.m");
-s8 := ReadTwoDB("8T1-8,8,4-g3-2216438532.m");
-s16 := ReadTwoDB("16T5-8,8,4-g5-1887073602.m");
+s2 := ReadTwoDB("2T1-1,2,2-g0-2054041558.m");
+s4 := ReadTwoDB("4T1-1,4,4-g0-1484691028.m");
+s8 := ReadTwoDB("8T2-2,4,4-g1-1599757394.m");
+s16 := ReadTwoDB("16T4-4,4,4-g3-4131114635.m");
 
 /* degree 2 */
 
 s2 := Degree2(s2, q);
-/* time TwoVerifyBrutal(s2); */
-time TwoVerify(s2);
+time TwoVerifyBrutal(s2);
 F2<a2> := FunctionField(s2);
 phi2 := BelyiMap(s2);
 auts2 := FunctionFieldAutomorphisms(s2);
@@ -28,8 +27,7 @@ auts2 := FunctionFieldAutomorphisms(s2);
 
 f4 := GetCandidateFunctions(s4, s2)[1];
 s4 := LiftBelyiMap(s4, s2, f4);
-/* time TwoVerifyBrutal(s4); */
-time TwoVerify(s4);
+time TwoVerifyBrutal(s4);
 F4<a> := FunctionField(s4);
 phi4 := BelyiMap(s4);
 auts4 := FunctionFieldAutomorphisms(s4);
@@ -39,7 +37,6 @@ brutal_auts4 := AutsFixingBaseField(F4);
 
 f8 := GetCandidateFunctions(s8, s4)[1];
 s8 := LiftBelyiMap(s8, s4, f8);
-/* time TwoVerifyBrutal(s8); */
 time TwoVerify(s8);
 F8 := FunctionField(s8);
 phi8 := BelyiMap(s8);
@@ -47,7 +44,7 @@ auts8 := FunctionFieldAutomorphisms(s8);
 
 /* degree 16 */
 
-f16 := GetCandidateFunctions(s16, s8)[2];
+f16 := GetCandidateFunctions(s16, s8)[1];
 s16 := LiftBelyiMap(s16, s8, f16);
 time TwoVerify(s16);
 F16 := FunctionField(s16);
