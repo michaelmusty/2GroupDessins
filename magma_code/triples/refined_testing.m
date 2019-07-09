ffe51a2d798e5b5a66fe879941a669cc9a094fd9
michaelmusty <michaelmusty@gmail.com>
2019-07-09 14:46:16 -0400

    spot check

---
+++ magma_code/triples/refined_testing.m
@@ -0,0 +1,48 @@
+intrinsic SpotCheck(s::TwoDB) -> Any
+  {}
+  printf "downstairs: %o\n", Name(s);
+  printf "computing lifts: ";
+  t0 := Cputime();
+  above := TwoDBToLifts(s);
+  t1 := Cputime();
+  printf "%o s\n", t1-t0;
+  printf "# lifts = %o\n", #above;
+  printf "computing refined passports\n";
+  for i := 1 to #above do
+    s := above[i];
+    printf "%o (%o out of %o): ", Name(s), i, #above;
+    t0 := Cputime();
+    refined_passport := RefinedPassport(PermutationTriple(s));
+    t1 := Cputime();
+    printf "%o s\n", t1-t0;
+  end for;
+  return above;
+end intrinsic;
+
+intrinsic SpotCheck(s::TwoDB, l::RngIntElt) -> Any
+  {}
+  path := [];
+  printf "start: %o\n", Name(s);
+  below := s;
+  for i := 1 to l do
+    printf "computing lifts of %o: ", Name(below);
+    t0 := Cputime();
+    above := TwoDBToLifts(below);
+    t1 := Cputime();
+    printf "%o s\n", t1-t0;
+    printf "there are %o lifts of %o\n", #above, Name(below);
+    printf "picking one at random\n";
+    Append(~path, below);
+    below := Random(above);
+  end for;
+  printf "computing refined passports for path:\n%o\n", path;
+  for i := 1 to #path do
+    s := path[i];
+    printf "%o (%o out of %o): ", Name(s), i, #path;
+    t0 := Cputime();
+    refined_passport := RefinedPassport(PermutationTriple(s));
+    t1 := Cputime();
+    printf "%o s\n", t1-t0;
+  end for;
+  return path;
+end intrinsic;
--- scripts/070919.m
+++ scripts/070919.m
@@ -1,1 +1,10 @@
 load "config.m";
+SetVerbose("TwoDB", 2);
+SetVerbose("TwoDBPassport", 1);
+d := 256;
+filenames := Filenames(d);
+filename := Random(filenames);
+print filename;
+s := ReadTwoDB(filename);
+
+SpotCheck(s, 3);
