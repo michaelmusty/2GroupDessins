40e3954a76a8758ca1e71b79d625276abc303a56
michaelmusty <michaelmusty@gmail.com>
2019-07-10 10:10:26 -0400

    rerefined

--- scripts/rerefined.m
+++ scripts/rerefined.m
@@ -6,10 +6,11 @@ degrees := [2,4,8,16,32,64,128,256];
 for d in degrees do
   objs := [ReadTwoDB(f) : f in Filenames(d)];
   for s in objs do
+    printf "%o:\n", Name(s);
     t0 := Cputime();
     pass := RefinedPassportJV(PermutationTriple(s));
     t1 := Cputime();
-    printf "%o:\nsize %o : %o s\n\n", Name(s), #pass, t1-t0;
+    printf "size %o : %o s\n\n", #pass, t1-t0;
     if #pass gt 1 then
       Append(~size, s);
     end if;
